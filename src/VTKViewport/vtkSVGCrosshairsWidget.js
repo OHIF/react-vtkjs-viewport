75f99e6725d2e8208781c05bf05786a64169d740
igoroctaviano <igoroctaviano@gmail.com>
2020-04-29 10:29:12 -0300

    Remove conditional in movecrosshairs

--- src/VTKViewport/vtkSVGCrosshairsWidget.js
+++ src/VTKViewport/vtkSVGCrosshairsWidget.js
@@ -151,31 +151,28 @@ function vtkSVGCrosshairsWidget(publicAPI, model) {
     }
 
     // Set camera focal point to world coordinate for linked views
-    apis.forEach((api, viewportIndex) => {
+    apis.forEach(api => {
       api.set('cachedCrosshairWorldPosition', worldPos);
+      // We are basically doing the same as getSlice but with the world coordinate
+      // that we want to jump to instead of the camera focal point.
+      // I would rather do the camera adjustment directly but I keep
+      // doing it wrong and so this is good enough for now.
+      const renderWindow = api.genericRenderWindow.getRenderWindow();
 
-      if (viewportIndex !== apiIndex) {
-        // We are basically doing the same as getSlice but with the world coordinate
-        // that we want to jump to instead of the camera focal point.
-        // I would rather do the camera adjustment directly but I keep
-        // doing it wrong and so this is good enough for now.
-        const renderWindow = api.genericRenderWindow.getRenderWindow();
+      const istyle = renderWindow.getInteractor().getInteractorStyle();
+      const sliceNormal = istyle.getSliceNormal();
+      const transform = vtkMatrixBuilder
+        .buildFromDegree()
+        .identity()
+        .rotateFromDirections(sliceNormal, [1, 0, 0]);
 
-        const istyle = renderWindow.getInteractor().getInteractorStyle();
-        const sliceNormal = istyle.getSliceNormal();
-        const transform = vtkMatrixBuilder
-          .buildFromDegree()
-          .identity()
-          .rotateFromDirections(sliceNormal, [1, 0, 0]);
+      const mutatedWorldPos = worldPos.slice();
+      transform.apply(mutatedWorldPos);
+      const slice = mutatedWorldPos[0];
 
-        const mutatedWorldPos = worldPos.slice();
-        transform.apply(mutatedWorldPos);
-        const slice = mutatedWorldPos[0];
+      istyle.setSlice(slice);
 
-        istyle.setSlice(slice);
-
-        renderWindow.render();
-      }
+      renderWindow.render();
 
       const renderer = api.genericRenderWindow.getRenderer();
       const wPos = vtkCoordinate.newInstance();
