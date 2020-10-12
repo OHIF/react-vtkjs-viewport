a46213d4f10dc054a429f7844551bd7ce4ec889b
James A. Petts <jamesapetts@gmail.com>
2020-10-12 11:37:40 +0100

    fix: 🐛 SVG orientation fix

--- examples/VTKRotatableCrosshairsExample.js
+++ examples/VTKRotatableCrosshairsExample.js
@@ -13,10 +13,12 @@ import vtkVolume from 'vtk.js/Sources/Rendering/Core/Volume';
 import vtkVolumeMapper from 'vtk.js/Sources/Rendering/Core/VolumeMapper';
 
 const url = 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs';
+
 const studyInstanceUID =
-  '1.3.6.1.4.1.14519.5.2.1.2744.7002.373729467545468642229382466905';
-const ctSeriesInstanceUID =
-  '1.3.6.1.4.1.14519.5.2.1.2744.7002.182837959725425690842769990419';
+  '1.3.12.2.1107.5.2.32.35162.30000015050317233592200000046';
+const mrSeriesInstanceUID =
+  '1.3.12.2.1107.5.2.32.35162.1999123112191238897317963.0.0.0';
+
 const searchInstanceOptions = {
   studyInstanceUID,
 };
@@ -74,7 +76,7 @@ class VTKRotatableCrosshairsExample extends Component {
     const imageIds = await createStudyImageIds(url, searchInstanceOptions);
 
     let ctImageIds = imageIds.filter(imageId =>
-      imageId.includes(ctSeriesInstanceUID)
+      imageId.includes(mrSeriesInstanceUID)
     );
 
     const ctImageDataObject = loadDataset(ctImageIds, 'ctDisplaySet');
